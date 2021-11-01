f3ba4fd0d0f5662c649dda5f024dc54facc8016e
eze217 <ezequielstella@gmail.com>
2021-11-01 18:00:09 +0100

    agrego view con ficha del proveedor, funcion cambio de estado ACT/INAC

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import proveedores
+from .views import proveedores,proveedor_ficha,inactivo_proveedor
 
 
 
 urlpatterns = [
     
-    path('', proveedores , name='proveedores')
+    path('', proveedores , name='proveedores'),
+    path('ficha_proveedor/<int:id_proveedor>', proveedor_ficha, name='proveedor_ficha'),
+    path('inactivo_proveedor/<int:id_proveedor>',inactivo_proveedor, name='inactivo_proveedor')
 
 ]
 
--- proveedores/views.py
+++ proveedores/views.py
@@ -1,12 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import *
 from .models import Proveedores
 
 # Create your views here.
 
 
 def proveedores(request):
+    #muestro los proveedores por defecto al admin
+   
+    if request.method == 'GET':
+        lista_proveedores = Proveedores.objects.all()
+        context = {'proveedores': lista_proveedores}
+        return render(request, 'proveedoresApp/proveedores.html', context)
 
-    lista_proveedores = Proveedores.objects.all()
 
-    data = {'proveedores': lista_proveedores}
-    return render(request, 'proveedoresApp/proveedores.html', data)
+#Ficha completa proveedor
+def proveedor_ficha(request,id_proveedor):
+
+    if request.method == 'GET':
+        proveedor= Proveedores.objects.get(id=id_proveedor)
+        context = {
+            'proveedor' : proveedor
+        }
+
+        return render(request, 'proveedoresApp/proveedor_ficha.html',context)
+
+#Pongo inactivo al proveedor
+
+
+def inactivo_proveedor(request, id_proveedor):
+    
+    if request.method == 'GET':
+        proveedor = Proveedores.objects.get(id=id_proveedor)
+        
+        proveedor.estado_proveedor = not (proveedor.estado_proveedor)
+        
+
+        proveedor.save()
+        
+        
+        context = {
+            'proveedor': proveedor
+        }
+
+        return redirect('proveedor_ficha',id_proveedor)
+
+
